eb78c88b941b46efc0bd71539f06dbe72733a377
Svetlana Romanova <svetlana.ivan.romanova@gmail.com>
2025-11-13 12:48:20 +0300

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""
+Minimal rocket health monitoring demo:
+- simple detectors for point anomalies (Z-score, EWMA, IsolationForest)
+- basic precision/recall/F1 evaluation against labeled points
+"""
+__all__ = []
